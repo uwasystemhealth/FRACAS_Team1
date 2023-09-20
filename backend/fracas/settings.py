1df84de53dc9cb105f33db4f2a4f791a5fc499a8
James Patrick Braunagel <jpatrickbraunagel@gmail.com>
2023-09-20 13:06:05 +0800

    remove CORS permissions

--- backend/fracas/settings.py
+++ backend/fracas/settings.py
@@ -35,7 +35,6 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -115,15 +114,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 # Authentication settings
 AUTH_USER_MODEL = "api.User"
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
-    ),
-}
 
 LANGUAGE_CODE = "en-us"
 
