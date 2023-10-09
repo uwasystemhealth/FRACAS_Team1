45d466b672a4c3d81581cacbcc22e1a33c0ed008
YanchenZhao233 <819413991zhao@gmail.com>
2023-10-10 05:48:39 +0800

    Removed unlinked editors #66

--- backend/api/models.py
+++ backend/api/models.py
@@ -149,7 +149,6 @@ class Record(models.Model):
     )
     record_owner_unlinked = models.TextField(null=True, blank=True)
     record_editors = models.ManyToManyField(User, blank=True)
-    record_editors_unlinked = models.TextField(null=True, blank=True)
     team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True)
     team_unlinked = models.TextField(blank=True, null=True)
     subsystem = models.ForeignKey(
