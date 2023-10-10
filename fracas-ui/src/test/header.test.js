ad9e38b7375a3467e34d628e77ea99c2f851f16e
stanley ser <stanleyfluke@gmail.com>
2023-10-10 13:05:23 +0800

    commit

--- fracas-ui/src/test/header.test.js
+++ fracas-ui/src/test/header.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from '../components/Header';
