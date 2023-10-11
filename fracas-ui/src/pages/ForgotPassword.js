3486463a16558bc90ac1ca7b7ee3f3b03c005564
James Patrick Braunagel <jpatrickbraunagel@gmail.com>
2023-10-11 16:30:46 +0800

    update forgotten password

--- fracas-ui/src/pages/ForgotPassword.js
+++ fracas-ui/src/pages/ForgotPassword.js
@@ -1,61 +1,75 @@
-import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import '../styles/ForgotPassword.scss';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../styles/ForgotPassword.scss";
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
 
-    const [email, setEmail] = useState('');
-    const [confirmMessage, setConfirmMessage] = useState(false)
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const handleSubmit = async () => {
-        try {
-            const response = await fetch("http://127.0.0.1:8000/auth/users/reset_email/", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    email: email,
-                }),
-            });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!email) {
+        alert('Please enter your email.');
+        return;
+      }
+    try {
+      const response = fetch("http://127.0.0.1:8000/auth/users/reset_email/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+        }),
+      });
 
-            if (response.status === 204) {
-                console.log("enter")
-                setConfirmMessage(true)
-            } else {
-                alert("An error occurred while sending the email.");
-            }
-        } catch (error) {
-            console.error("Error while registering:", error);
-            alert("An error occurred. Please try again.");
-        }
+      if (response.status === 204) {
+        console.log("enter");
+      }
+      alert(
+        "Supplied email will be sent a reset link if it is associated with an account."
+      );
+      navigate("/");
+    } catch (error) {
+      console.error("Error while registering:", error);
+      alert("An error occurred. Please try again.");
     }
-    return (
-        <div className='fp-container'>
-            <div className='top-img'>
-                <h1>FRACAS <br /> FORGOT PASSWORD</h1>
+  };
+  return (
+    <div className="fp-container">
+      <div className="top-img">
+        <h1>
+          FRACAS <br /> FORGOT PASSWORD
+        </h1>
+      </div>
+      <h1 className="fp-w fp-heading">Forgot Password</h1>
+      <div className="fp-box fp-w">
+        <div className="inpbox fp-w">
+          <form onSubmit={handleSubmit}>
+            <div className="inp">
+              <span>Email</span>
+              <input
+                type="text"
+                placeholder="Please enter your Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-            <h1 className='fp-w fp-heading'>Forgot Password</h1>
-            <div className='fp-box fp-w'>
-                <div className='inpbox fp-w'>
-                    <div className="inp">
-                        <span>Email</span>
-                        <input type="text" placeholder="Please enter your Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-                    <button onClick={handleSubmit}>Submit</button>
-                    {confirmMessage === true && (
-                        <div className='confirm'>
-                            <span>An email to reset your password has been sent to your registered email id.</span>
-                        </div>
-                    )}
-                    <div className='create'>
-                        Don't have an account? <Link className='link' to="/signup">Create account</Link>
-                    </div>
-                </div>
-            </div>
+            <button type="submit">
+              Submit
+            </button>
+          </form>
+          <div className="create">
+            Don't have an account?{" "}
+            <Link className="link" to="/signup">
+              Create account
+            </Link>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default ForgotPassword;
